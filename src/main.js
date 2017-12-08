14c2a4ace88c38e388453c1f19ffe83d86577ff4
Ralph Effting <ralfting@gmail.com>
2017-12-08 10:43:29 -0200

    Create AlbumInfo component

--- src/main.js
+++ src/main.js
@@ -1,10 +1,16 @@
 import spotify from './Spotify';
 import renderAlbums from './AlbumList';
+import renderAlbumInfo from './AlbumInfo';
 
-const albums = spotify.search.albums('metallica');
-const albumsListElement = document.getElementById('albums-list');
-
-
+const albums = spotify.search.albums('Green Day');
+const albumsList = document.getElementById('albums-list');
 albums
   .then(response => response.json())
-  .then(dataJson => renderAlbums(dataJson.albums.items, albumsListElement));
+  .then(dataJson => renderAlbums(dataJson.albums.items, albumsList));
+
+const idAlbum = '6peEdPVO73WtgGah5sEhX4';
+const album = spotify.album.getAlbum(idAlbum);
+const albumInfo = document.getElementById('album-info');
+album
+  .then(response => response.json())
+  .then(dataJson => renderAlbumInfo(dataJson, albumInfo));
